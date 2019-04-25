4161d6c562ff06ee5184676553d518282981189a
echeck <jianda.su@51echeck.com>
2019-04-25 16:04:22 +0800

    20190425第三次提交

--- echecklibrary/src/main/java/com/su/echeck/echecklibrary/base/BaseApplication.java
+++ echecklibrary/src/main/java/com/su/echeck/echecklibrary/base/BaseApplication.java
@@ -2,7 +2,6 @@ package com.su.echeck.echecklibrary.base;
 
 import android.app.Application;
 
-import com.google.code.microlog4android.config.PropertyConfigurator;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.cache.CacheEntity;
 import com.lzy.okgo.cache.CacheMode;
@@ -21,8 +20,6 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        PropertyConfigurator.getConfigurator(this).configure();
         initOkGo();
     }
 
